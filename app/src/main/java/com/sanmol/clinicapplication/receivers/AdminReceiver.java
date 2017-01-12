35a37a1ff635e702458291554cb231bdd3b84242
mhshinde <mayurihiralal@gmail.com>
2017-01-12 19:10:09 +0530

    mayuri is fighting with exit issue

---
+++ app/src/main/java/com/sanmol/clinicapplication/receivers/AdminReceiver.java
@@ -0,0 +1,10 @@
+package com.sanmol.clinicapplication.receivers;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by Sanmol on 1/12/2017.
+ */
+
+public class AdminReceiver extends DeviceAdminReceiver {
+}
